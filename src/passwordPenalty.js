9b200affd899ff68f4e543354189e6fc318a4304
ll-sie <e.ndiramiye@alustudent.com>
2023-10-09 17:08:51 +0200

    Making penalty points

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -7,10 +7,25 @@
  */
 export default function penaltyPoints(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
-  if (typeof password !== "string") password = String(password);
+  if (password === null || password === undefined){
+    return 0;
+  } else {
+    if (typeof password !== "string") password = String(password);
+    const regex = /(.)\1+/g;
+    
+    const matches = password.match(regex);
+    
+    let repetitionCounts = [];
 
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  // * * * INSERT YOUR CODE HERE * * * * * * * * * * * * * *
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  //
+    if (matches) {
+      repetitionCounts = matches
+        .filter(match => match.length > 1) // Filter out non-repeating characters
+        .map(match => match.length)
+        .map(match => 
+        (match === 2) ? 1 : 2);
+    }
+
+    const sum = repetitionCounts.reduce((a, b) => a + b, 0); // Initialize the reduce with 0
+    return sum || 0;
+  }
 }
